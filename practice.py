55079bec7d65ef99b7835dcf4eaef2fdc97f6e98
KREETHIC G M <kreethickreethic26@gmail.com>
2026-03-15 17:18:33 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,66 @@
+'''def hey():
+    sum=a+b
+    print(sum)
+
+a=int(input())
+b=int(input())
+hey()
+
+num=[1,2,3,4,5]
+even =list(filter(lambda x:x%2==0,num))
+print(even)
+
+# lowercase to uppercase:
+a="kreethic"
+print(a.upper())
+
+#sentence uppercase to lowercase:
+a="Hi Iam KREETHIC!"
+print(a.lower())
+
+#capitalization:
+a=input()
+print(a.capitalize())
+
+#format:
+name=input()
+age=int(input())
+print({name}," age is ",{age})
+
+#index:
+a="Kreethic"
+print(a.index('e'))
+
+#check ends:
+a="Kreethic"
+print(a.endswith("ic"))
+
+#expand tabs:
+a="hello \t world"
+print(a.expandtabs())
+
+
+#encode:
+a="Kreethic"
+print(a.encode('utf-8'))
+
+#check is digit:
+a="Kreethic"
+print(a.isdigit())
+
+#check numeric:
+a="Kreethic"
+print(a.isnumeric())
+
+#check alphanumeric:
+
+a="Kreethic26"
+print(a.isalnum())
+
+#check ASCII:
+a="?/.,"
+print(a.isascii())'''
+
+#check Alphabets:
+a="Kreethic"
+print(a.isalpha())
